54e923e17696aeb2daad89a60f1249460021c30a
zhangxiaofeng <zhangxiaofeng@zbj.com>
2020-11-30 17:22:02 +0800

    hehe

--- bin/start-canal2kafka.sh
+++ bin/start-canal2kafka.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-yarn jar pipeline-1.0.jar com.zbj.finance.datapipeline.mr.Canal2kafka conf/  lib/
+yarn jar pipeline-1.0.jar com.mycompany.bigdata.datapipeline.mr.Canal2kafka conf/  lib/
--- bin/start-kafka2hive.sh
+++ bin/start-kafka2hive.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-yarn jar pipeline-1.0.jar com.zbj.finance.datapipeline.mr.Kafka2Hive conf/  lib/  1
+yarn jar pipeline-1.0.jar com.mycompany.bigdata.datapipeline.mr.Kafka2Hive conf/  lib/  1
